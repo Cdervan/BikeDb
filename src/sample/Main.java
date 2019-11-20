d705d59db90db4cbb25735729f74fdb8a0e7be8a
davidstevenrose <33883868+davidstevenrose@users.noreply.github.com>
2019-11-20 18:12:22 -0500

    Update Main.java
    Added some documentation to your Main.java file for credibility.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+/**
+ *
+ * The {@code Application} class wraps the loading of javafx components
+ * defined in <i>fxml</i> source. {@code Application} is the driver class
+ * for the application.
+ *
+ * <p>In addition, this class provides several lines of code for launching
+ * a GUI composed of {@code javafx.scene.Node} objects .
+ *
+ * @author  Scott Vanselow
+ * @author  David Rose (editor)
+ * @since   JDK13.1
+ */
 public class Main extends Application {
 
     @Override
